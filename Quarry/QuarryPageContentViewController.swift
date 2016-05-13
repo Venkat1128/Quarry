09e1739e454e149b8563a6f17d0a1d8e50ba512a
Venkat <venkat@giffgaff.co.uk>
2016-05-13 17:21:50 +0100

    implementing page controller

---
+++ Quarry/QuarryPageContentViewController.swift
@@ -0,0 +1,37 @@
+//
+//  QuarryPageContentViewController.swift
+//  Quarry
+//
+//  Created by Venkat on 13/05/16.
+//  Copyright © 2016 NTT DATA. All rights reserved.
+//
+
+import UIKit
+
+class QuarryPageContentViewController: UIViewController {
+
+    @IBOutlet weak var topicpptImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
